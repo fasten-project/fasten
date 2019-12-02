5eab0bebb5c8256b1b4907a8e3ba785df828108a
mir-am <mir-am@hotmail.com>
2019-12-02 11:01:40 +0100

    A utility class for loading Jar files and using their class and methods.

--- analyzer/javacg-opal/src/main/java/eu/fasten/analyzer/javacgopal/plugins/Analyzer.java
+++ analyzer/javacg-opal/src/main/java/eu/fasten/analyzer/javacgopal/plugins/Analyzer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import picocli.CommandLine;
 
 /**
  * Currently, this is a dummy analyzer, which shows Maven coordinates that were produced.
@@ -47,5 +48,24 @@ public class Analyzer implements KafkaConsumer<String>{
             logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
         }
 
+    }
+}
+
+@CommandLine.Command(name = "MavenCoordConsumer", description = "Generates call graphs from Maven coordinates.")
+class Main implements Runnable{
+
+    @CommandLine.Option(names = {"-h", "--host"}, defaultValue = "localhost", description = "The IP address of the Kafka server")
+    String IP;
+    @CommandLine.Option(names = {"-p", "--port"}, defaultValue = "9092", description = "The port of the Kafka server.")
+    String port;
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
     }
 }
