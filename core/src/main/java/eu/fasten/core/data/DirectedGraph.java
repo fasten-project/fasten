71ee2b7db8c865b2a32fbcf04d2a623c424e393c
Paolo Boldi <boldi@dsi.unimi.it>
2020-04-17 11:16:56 +0200

    Tweaking

---
+++ core/src/main/java/eu/fasten/core/data/DirectedGraph.java
@@ -0,0 +1,14 @@
+package eu.fasten.core.data;
+
+import it.unimi.dsi.fastutil.longs.LongList;
+import it.unimi.dsi.fastutil.longs.LongSet;
+
+public interface DirectedGraph {
+	public int numNodes();
+	public long numArcs();
+	public LongList successors(final long node);
+	public LongList predecessors(final long node);
+	public LongList nodes();
+	public LongSet externalNodes();
+
+}
