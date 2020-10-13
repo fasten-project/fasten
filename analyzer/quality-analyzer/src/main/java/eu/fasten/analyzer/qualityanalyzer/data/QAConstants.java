2babac4e34bf381be21e4655da1f6c763ea13ef2
Miroslav Zivkovic <mzivkovic@telfort.nl>
2020-10-13 09:11:40 +0200

    Single callable (meta)data is consumed per message

---
+++ analyzer/quality-analyzer/src/main/java/eu/fasten/analyzer/qualityanalyzer/data/QAConstants.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.fasten.analyzer.qualityanalyzer.data;
+
+public class QAConstants {
+
+    public static final String QA_VERSION_NUMBER = "0.0.1";
+
+    public static final String MVN_FORGE = "mvn";
+    public static final String JAVA_FORGE = "java";
+    public static final String C_FORGE = "C";
+    public static final String PyPI_FORGE = "PyPI";
+
+    //field names in JSON Object sent by Rapid
+    public static final String LINE_START = "line_start";
+    public static final String LINE_END = "line_end";
+    public static final String RETURN_TYPE = "return_type";
+    public static final String FASTEN_URI = "fasten_uri";
+    public static final String MODULE_ID = "module_id";
+
+
+    public static final int transactionRestartLimit = 3;
+
+    public static final int insertionBatchSize = 4096;
+
+    public static final String fastenDbPassEnvVariable = "FASTEN_DBPASS";
+
+}
