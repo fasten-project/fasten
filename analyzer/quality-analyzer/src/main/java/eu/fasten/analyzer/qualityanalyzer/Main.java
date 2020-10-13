4f4da13fe8e2056c890691b029c290192b15af45
Miroslav Zivkovic <mzivkovic@telfort.nl>
2020-10-13 09:07:03 +0200

    Single callable (meta)data is consumed per message

--- analyzer/quality-analyzer/src/main/java/eu/fasten/analyzer/qualityanalyzer/Main.java
+++ analyzer/quality-analyzer/src/main/java/eu/fasten/analyzer/qualityanalyzer/Main.java
@@ -20,10 +20,17 @@ package eu.fasten.analyzer.qualityanalyzer;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import eu.fasten.server.connectors.PostgresConnector;
+
+import org.jooq.DSLContext;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "Quality Analyzer")
@@ -33,9 +40,21 @@ public class Main implements Runnable {
 
     @CommandLine.Option(names = {"-f", "--file"},
             paramLabel = "JSON File",
-            description = "Path to JSON file which contains quality metrics and artifactId, "
-                    + "groupId and version")
+            description = "Path to JSON file that contains JSON with Lizard tool generated quality metrics")
+
     String jsonFile;
+
+    @CommandLine.Option(names = {"-d", "--database"},
+            paramLabel = "DB_URL",
+            description = "Database URL for connection",
+            defaultValue = "jdbc:postgresql:postgres")
+    String dbUrl;
+
+    @CommandLine.Option(names = {"-u", "--user"},
+            paramLabel = "DB_USER",
+            description = "Database user name",
+            defaultValue = "postgres")
+    String dbUser;
 
     public static void main(String[] args) {
         final int exitCode = new CommandLine(new Main()).execute(args);
@@ -45,16 +64,42 @@ public class Main implements Runnable {
     @Override
     public void run() {
         var qualityAnalyser = new QualityAnalyzerPlugin.QualityAnalyzer();
+
+        HashMap<String, DSLContext> hm = new HashMap<String, DSLContext>();
+        try {
+            var dbUrl = "jdbc:postgresql:fasten_java";
+            var dbUser = "fasten";
+            var context = PostgresConnector.getDSLContext(dbUrl, dbUser);
+            hm.put("mvn", context);
+            hm.put("java", context);
+
+            dbUrl = "jdbc:postgresql:fasten_c";
+            dbUser = "fasten";
+            context = PostgresConnector.getDSLContext(dbUrl, dbUser);
+            hm.put("d", context);
+
+            dbUrl = "jdbc:postgresql:fasten_python";
+            dbUser = "fasten";
+            context = PostgresConnector.getDSLContext(dbUrl, dbUser);
+            hm.put("python", context);
+        } catch(SQLException e) {
+            logger.error("Could not connect to the database " +dbUrl, e);
+            return;
+        }
+
+        MetadataUtils mu = new MetadataUtils(hm);
+
         final FileReader reader;
+
         try {
             reader = new FileReader(jsonFile);
         } catch (FileNotFoundException e) {
-            logger.error("Could not find input JSON " + jsonFile, e);
+            logger.error("Could not find input JSON file " + jsonFile, e);
             return;
         }
+
         final JSONObject json = new JSONObject(new JSONTokener(reader));
         qualityAnalyser.consume(json.toString());
-        var resultOptional = qualityAnalyser.produce();
-        resultOptional.ifPresent(System.out::println);
+        qualityAnalyser.produce().ifPresent(System.out::println);
     }
 }
