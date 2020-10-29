9341ecf57eeb2a9775712aaa56cdf2ef9987fddf
MihhailSokolov <m.sokolov@student.tudelft.nl>
2020-10-29 16:37:40 +0200

    Make pagination optional

--- core/src/main/java/eu/fasten/core/maven/DependencyGraphBuilder.java
+++ core/src/main/java/eu/fasten/core/maven/DependencyGraphBuilder.java
@@ -40,6 +40,16 @@ import java.util.List;
 
 public class DependencyGraphBuilder {
 
+    private final boolean usePagination;
+
+    public DependencyGraphBuilder() {
+        this(false);
+    }
+
+    public DependencyGraphBuilder(boolean usePagination) {
+        this.usePagination = usePagination;
+    }
+
     public static void main(String[] args) throws SQLException {
         var dbContext = PostgresConnector.getDSLContext("jdbc:postgresql://localhost:5433/fasten_java", "fasten");
         var graphBuilder = new DependencyGraphBuilder();
@@ -52,6 +62,14 @@ public class DependencyGraphBuilder {
     private static final Logger logger = LoggerFactory.getLogger(DependencyGraphBuilder.class);
 
     public Graph<DependencyNode, DependencyEdge> buildDependencyGraph(DSLContext dbContext) {
+        if (this.usePagination) {
+            return this.buildDependencyGraphWithPagination(dbContext);
+        } else {
+            return this.buildDependencyGraphWithoutPagination(dbContext);
+        }
+    }
+
+    private Graph<DependencyNode, DependencyEdge> buildDependencyGraphWithPagination(DSLContext dbContext) {
         logger.info("Obtaining dependency data and generating global dependency graph");
         var dependencyGraph = new DefaultDirectedGraph<DependencyNode, DependencyEdge>(DependencyEdge.class);
         long lastFetchedArtifact = 0;
@@ -118,7 +136,7 @@ public class DependencyGraphBuilder {
         logger.info("Obtaining timestamps of artifacts to enrich dependency graph");
         lastFetchedArtifact = 0;
         fetchNext = true;
-        while(fetchNext) {
+        while (fetchNext) {
             var timestampsResult = dbContext
                     .select(PackageVersions.PACKAGE_VERSIONS.ID,
                             Packages.PACKAGES.PACKAGE_NAME,
@@ -159,6 +177,91 @@ public class DependencyGraphBuilder {
                 if (timestampedArtifacts.containsKey(node.artifact)) {
                     node.updateTimestamp(timestampedArtifacts.get(node.artifact));
                 }
+            }
+        }
+        logger.info("Successfully generated ecosystem-wide dependency graph");
+        return dependencyGraph;
+    }
+
+    private Graph<DependencyNode, DependencyEdge> buildDependencyGraphWithoutPagination(DSLContext dbContext) {
+        logger.info("Obtaining timestamps of artifacts for generating dependency graph");
+        var timestampsResult = dbContext
+                .select(
+                        Packages.PACKAGES.PACKAGE_NAME,
+                        PackageVersions.PACKAGE_VERSIONS.VERSION,
+                        PackageVersions.PACKAGE_VERSIONS.CREATED_AT)
+                .from(Packages.PACKAGES)
+                .join(PackageVersions.PACKAGE_VERSIONS)
+                .on(PackageVersions.PACKAGE_VERSIONS.PACKAGE_ID.eq(Packages.PACKAGES.ID))
+                .where(Packages.PACKAGES.FORGE.eq(Constants.mvnForge))
+                .and(Packages.PACKAGES.ID.greaterThan(0L))
+                .fetch();
+        var timestampedArtifacts = new HashMap<Dependency, Timestamp>();
+        for (var record : timestampsResult) {
+            var mavenCoordinate = record.component1() + Constants.mvnCoordinateSeparator + record.component2();
+            var timestamp = record.component3();
+            if (timestamp == null) {
+                timestamp = new Timestamp(-1);
+            }
+            try {
+                timestampedArtifacts.put(new Dependency(mavenCoordinate), timestamp);
+            } catch (IllegalArgumentException e) {
+                logger.error("Error parsing Maven coordinate '" + mavenCoordinate + "'", e);
+            }
+        }
+        timestampsResult = null;
+        logger.info("Obtaining dependency data for generating dependency graph");
+        var dependenciesResult = dbContext
+                .select(Packages.PACKAGES.PACKAGE_NAME,
+                        PackageVersions.PACKAGE_VERSIONS.VERSION,
+                        Dependencies.DEPENDENCIES.METADATA)
+                .from(Packages.PACKAGES)
+                .join(PackageVersions.PACKAGE_VERSIONS)
+                .on(PackageVersions.PACKAGE_VERSIONS.PACKAGE_ID.eq(Packages.PACKAGES.ID))
+                .join(Dependencies.DEPENDENCIES)
+                .on(Dependencies.DEPENDENCIES.PACKAGE_VERSION_ID.eq(PackageVersions.PACKAGE_VERSIONS.ID))
+                .where(Packages.PACKAGES.FORGE.eq(Constants.mvnForge))
+                .fetch();
+        if (dependenciesResult == null || dependenciesResult.isEmpty()) {
+            return null;
+        }
+        var dependencies = new HashMap<Dependency, List<Dependency>>();
+        for (var record : dependenciesResult) {
+            var mavenCoordinate = record.component1() + Constants.mvnCoordinateSeparator + record.component2();
+            var artifact = new Dependency(mavenCoordinate);
+            var dependency = Dependency.fromJSON(new JSONObject(record.component3().data()));
+            var depList = dependencies.get(artifact);
+            if (depList == null) {
+                dependencies.put(artifact, List.of(dependency));
+            } else {
+                var newDepList = new ArrayList<>(depList);
+                newDepList.add(dependency);
+                dependencies.put(artifact, newDepList);
+            }
+        }
+        dependenciesResult = null;
+        logger.info("Generating global dependency graph");
+        var dependencyGraph = new DefaultDirectedGraph<DependencyNode, DependencyEdge>(DependencyEdge.class);
+        for (var entry : timestampedArtifacts.entrySet()) {
+            dependencyGraph.addVertex(new DependencyNode(entry.getKey(), entry.getValue()));
+        }
+        long id = 0;
+        for (var entry : dependencies.entrySet()) {
+            var source = new DependencyNode(entry.getKey(), timestampedArtifacts.get(entry.getKey()));
+            if (!dependencyGraph.containsVertex(source)) {
+                dependencyGraph.addVertex(source);
+            }
+            var dependencyList = entry.getValue();
+            for (var dependency : dependencyList) {
+                var targetDependency = new Dependency(dependency.toMavenCoordinate());
+                var targetTimestamp = timestampedArtifacts.get(targetDependency) != null
+                        ? timestampedArtifacts.get(targetDependency) : new Timestamp(-1);
+                var target = new DependencyNode(targetDependency, targetTimestamp);
+                if (!dependencyGraph.containsVertex(target)) {
+                    dependencyGraph.addVertex(target);
+                }
+                var edge = new DependencyEdge(id++, dependency.scope, dependency.optional, dependency.exclusions);
+                dependencyGraph.addEdge(source, target, edge);
             }
         }
         logger.info("Successfully generated ecosystem-wide dependency graph");
