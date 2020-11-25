24d14792bf043aac3a638b7a5c217b4cb48057e8
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2020-11-25 17:11:52 +0100

    Tweaking

--- core/src/main/java/eu/fasten/core/examples/CallGraphAnalysisExample.java
+++ core/src/main/java/eu/fasten/core/examples/CallGraphAnalysisExample.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package eu.fasten.core.examples;
 
 import java.sql.SQLException;
@@ -19,9 +37,6 @@ import com.martiansoftware.jsap.Parameter;
 import com.martiansoftware.jsap.SimpleJSAP;
 import com.martiansoftware.jsap.UnflaggedOption;
 
-import it.unimi.dsi.fastutil.longs.LongArrays;
-import it.unimi.dsi.util.XoRoShiRo128PlusPlusRandom;
-
 import eu.fasten.core.data.Constants;
 import eu.fasten.core.data.DirectedGraph;
 import eu.fasten.core.data.FastenURI;
@@ -29,6 +44,8 @@ import eu.fasten.core.data.metadatadb.codegen.tables.Callables;
 import eu.fasten.core.data.metadatadb.codegen.tables.PackageVersions;
 import eu.fasten.core.data.metadatadb.codegen.tables.Packages;
 import eu.fasten.core.dbconnectors.PostgresConnector;
+import it.unimi.dsi.fastutil.longs.LongArrays;
+import it.unimi.dsi.util.XoRoShiRo128PlusPlusRandom;
 
 public class CallGraphAnalysisExample {
 
@@ -37,7 +54,7 @@ public class CallGraphAnalysisExample {
 	}
 
 	public static void main(final String args[]) throws JSAPException, IllegalArgumentException, SQLException, RocksDBException {
-		final SimpleJSAP jsap = new SimpleJSAP(CallGraphAnalysisExample.class.getName(), "Analyzes a revision call graph", new Parameter[] {
+		final SimpleJSAP jsap = new SimpleJSAP(CallGraphAnalysisExample.class.getName(), "Analyzes a revision call graph and print some statistics.", new Parameter[] {
 				new FlaggedOption("postgres", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'p', "postgres", "The URI of the Postgres server."),
 				new FlaggedOption("db", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'd', "db", "The Postgres database."),
 				new FlaggedOption("rocksdb", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'r', "rocksdb", "The path to the RocksDB graph database."),
@@ -68,7 +85,7 @@ public class CallGraphAnalysisExample {
 		if (graph == null) throw new IllegalArgumentException("The requested revision (group=" + group + ", product=" + product + ", version=" + version + ", id=" + id + ") is not in the RocksDB database");
 
 		// Find node with highest outdegree
-		long bestDegree = 0;
+		long bestDegree = -1;
 		long bestDNode = -1;
 		for (final long v : graph.nodes()) {
 			final long d = graph.outDegreeOf(v);
@@ -81,7 +98,7 @@ public class CallGraphAnalysisExample {
 		System.out.println("The callable with highest outdegree (" + bestDegree + ") is " + getCallableName(bestDNode, context) + " (id=" + bestDNode + ")");
 
 		// Find node with highest indegre
-		bestDegree = 0;
+		bestDegree = -1;
 		bestDNode = -1;
 		for (final long v : graph.nodes()) {
 			final long d = graph.inDegreeOf(v);
@@ -97,7 +114,7 @@ public class CallGraphAnalysisExample {
 		final PageRank<Long, long[]> pageRank = new PageRank<>(graph);
 
 		// Find node with highest PageRank
-		double bestPR = 0;
+		double bestPR = -1;
 		long bestPRNode = -1;
 		for (final long v : graph.nodes()) {
 			final double pr = pageRank.getVertexScore(v);
@@ -109,30 +126,14 @@ public class CallGraphAnalysisExample {
 
 		System.out.println("The callable with highest PageRank (" + bestPR + ") is " + getCallableName(bestPRNode, context) + " (id=" + bestPRNode + ")");
 
-		// Now we compute positive harmonic centrality (outgoing paths)
-		final HarmonicCentrality<Long, long[]> positiveHarmonicCentrality = new HarmonicCentrality<>(graph, false, false);
-		final Map<Long, Double> positiveHarmonicCentralityScores = positiveHarmonicCentrality.getScores();
-
-		// Find node with highest harmonic centrality
-		double bestH = 0;
-		long bestHNode = -1;
-		for (final long v : graph.nodes()) {
-			final double h = positiveHarmonicCentralityScores.get(v);
-			if (h > bestH) {
-				bestH = h;
-				bestHNode = v;
-			}
-		}
-
-		System.out.println("The callable with highest positive harmonic centrality (" + bestH + ") is " + getCallableName(bestHNode, context) + " (id=" + bestHNode + ")");
-
+		// We will need the transpose now. This is just a lightweight view.
 		final EdgeReversedGraph<Long, long[]> transpose = new EdgeReversedGraph<>(graph);
 
 		// Now we compute PageRank on the transpose
 		final PageRank<Long, long[]> transposePageRank = new PageRank<>(transpose);
 
 		// Find node with highest transpose PageRank
-		bestPR = 0;
+		bestPR = -1;
 		bestPRNode = -1;
 		for (final long v : graph.nodes()) {
 			final double pr = transposePageRank.getVertexScore(v);
@@ -144,12 +145,29 @@ public class CallGraphAnalysisExample {
 
 		System.out.println("The callable with highest transpose PageRank (" + bestPR + ") is " + getCallableName(bestPRNode, context) + " (id=" + bestPRNode + ")");
 
+		// Now we compute positive harmonic centrality (outgoing paths)
+		final HarmonicCentrality<Long, long[]> positiveHarmonicCentrality = new HarmonicCentrality<>(graph, false, false);
+		final Map<Long, Double> positiveHarmonicCentralityScores = positiveHarmonicCentrality.getScores();
+
+		// Find node with highest positive harmonic centrality
+		double bestH = -1;
+		long bestHNode = -1;
+		for (final long v : graph.nodes()) {
+			final double h = positiveHarmonicCentralityScores.get(v);
+			if (h > bestH) {
+				bestH = h;
+				bestHNode = v;
+			}
+		}
+
+		System.out.println("The callable with highest positive harmonic centrality (" + bestH + ") is " + getCallableName(bestHNode, context) + " (id=" + bestHNode + ")");
+
 		// Now we compute negative harmonic centrality (incoming paths)
 		final HarmonicCentrality<Long, long[]> negativeHarmonicCentrality = new HarmonicCentrality<>(graph, true, false);
 		final Map<Long, Double> negativeHarmonicCentralityScores = negativeHarmonicCentrality.getScores();
 
 		// Find node with highest harmonic centrality
-		bestH = 0;
+		bestH = -1;
 		bestHNode = -1;
 		for (final long v : graph.nodes()) {
 			final double h = negativeHarmonicCentralityScores.get(v);
@@ -161,10 +179,12 @@ public class CallGraphAnalysisExample {
 
 		System.out.println("The callable with highest negative harmonic centrality (" + bestH + ") is " + getCallableName(bestHNode, context) + " (id=" + bestHNode + ")");
 
+		// We now explore balls around node using a radius-based limit.
+
 		// We choose 10 random nodes (very inefficient, should be a Knuth-Yates shuffle)
 		final long[] node = Arrays.copyOf(LongArrays.shuffle(graph.nodes().toLongArray(), new XoRoShiRo128PlusPlusRandom(0)), 10);
 
-		for(long v: node) {
+		for (final long v : node) {
 			System.out.println();
 			System.out.println();
 			System.out.println("========= " + getCallableName(v, context) + " (id=" + v + ")" + " =========");
@@ -182,7 +202,7 @@ public class CallGraphAnalysisExample {
 			System.out.println("Finding nodes coreachable within distance 3 from " + getCallableName(v, context) + " (id=" + v + ")");
 			final ClosestFirstIterator<Long, long[]> coreachable = new ClosestFirstIterator<>(transpose, v, 3);
 			coreachable.forEachRemaining((x) -> {
-				System.out.println("\tFound node " + getCallableName(x, context) + " (id=" + x + ") at distance " +  coreachable.getShortestPathLength(x));
+				System.out.println("\tFound node " + getCallableName(x, context) + " (id=" + x + ") at distance " + coreachable.getShortestPathLength(x));
 			});
 		}
 
