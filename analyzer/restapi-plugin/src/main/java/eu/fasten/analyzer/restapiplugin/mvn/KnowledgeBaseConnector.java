288b93995ad3fd083a7dd49d440f5ea758ce62ba
MihhailSokolov <m.sokolov@student.tudelft.nl>
2020-12-01 16:32:58 +0200

    Remove unnecessary dependency on the server

--- analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/KnowledgeBaseConnector.java
+++ analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/KnowledgeBaseConnector.java
@@ -1,7 +1,7 @@
 package eu.fasten.analyzer.restapiplugin.mvn;
 
 import eu.fasten.core.data.metadatadb.MetadataDao;
-import eu.fasten.server.connectors.PostgresConnector;
+import eu.fasten.core.dbconnectors.PostgresConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
