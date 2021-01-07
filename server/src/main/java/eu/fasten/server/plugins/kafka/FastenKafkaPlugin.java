448d2124155dd7b8d2def3bd0a6c0f4307c4c336
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-01-07 16:03:30 +0100

    Get rid of shutdown hook, as it might interfere with static membership.

--- server/src/main/java/eu/fasten/server/plugins/kafka/FastenKafkaPlugin.java
+++ server/src/main/java/eu/fasten/server/plugins/kafka/FastenKafkaPlugin.java
@@ -75,7 +75,7 @@ public class FastenKafkaPlugin implements FastenServerPlugin {
 
         this.connection = new KafkaConsumer<>(consumerProperties);
         this.producer = new KafkaProducer<>(producerProperties);
-        
+
         handleConsumerShutdown();
 
         this.skipOffsets = skipOffsets;
@@ -370,13 +370,5 @@ public class FastenKafkaPlugin implements FastenServerPlugin {
         } while (i <= 5 && statusRecords.count() == 0);
 
         return statusRecords;
-    }
-
-    /**
-     * Add hook for graceful consumer shutdown.
-     */
-    private void handleConsumerShutdown() {
-        Thread closeConsumer = new Thread(() -> connection.close());
-        Runtime.getRuntime().addShutdownHook(closeConsumer);
     }
 }
