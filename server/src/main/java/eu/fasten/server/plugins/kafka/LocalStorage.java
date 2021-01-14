92ee64c80d46ed03c327c8adadcf9da4244f5931
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-01-14 13:47:34 +0100

    Add local storage test.

--- server/src/main/java/eu/fasten/server/plugins/kafka/LocalStorage.java
+++ server/src/main/java/eu/fasten/server/plugins/kafka/LocalStorage.java
@@ -1,11 +1,16 @@
 package eu.fasten.server.plugins.kafka;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
+import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_1;
 import java.io.File;
+import java.io.IOException;
 
 public class LocalStorage {
 
     private final String instanceId;
     private final File storageFolder;
+    private final DigestUtils digestUtils = new DigestUtils(SHA_1);
 
     public LocalStorage(String folder) {
         if (System.getenv("POD_INSTANCE_ID") != null) {
@@ -14,12 +19,46 @@ public class LocalStorage {
             throw new IllegalArgumentException("Trying to initialize local storage but $POD_INSTANCE_ID is not in the environemnt variables.");
         }
 
-        storageFolder = new File(folder);
+        storageFolder = new File(folder + File.separator + instanceId + File.separator);
 
         // Create folder if it doesn't exist yet.
         if (!storageFolder.exists()) {
             storageFolder.mkdirs();
         }
+    }
+
+    public boolean exists(String message) {
+        String hashedMessage = digestUtils.digestAsHex(message);
+        String[] filesInFolder = storageFolder.list();
+
+        for (String hash : filesInFolder) {
+            if (hash.equals(hashedMessage)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(String message) {
+        if (!exists(message)) {
+            return false;
+        }
+
+        String hashedMessage = digestUtils.digestAsHex(message);
+        File fileToRemove = new File(storageFolder.getPath() + File.separator + hashedMessage);
+
+        return fileToRemove.delete();
+    }
+
+    public boolean store(String message) throws IOException {
+        if (exists(message)) {
+            return false;
+        }
+
+        String hashedMessage = digestUtils.digestAsHex(message);
+        File fileToCreate = new File(storageFolder.getPath() + File.separator + hashedMessage);
+
+        return fileToCreate.createNewFile();
     }
 
 
---
+++ server/src/test/java/eu/fasten/server/plugins/kafka/LocalStorageTest.java
@@ -0,0 +1,86 @@
+package eu.fasten.server.plugins.kafka;
+
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class LocalStorageTest {
+
+    private LocalStorage localStorage;
+
+    @BeforeEach
+    public void setupStorage() throws URISyntaxException {
+        setEnv("POD_INSTANCE_ID", "test_pod");
+       localStorage = new LocalStorage(new File("src/test/resources").getAbsolutePath());
+    }
+
+    @AfterEach
+    public void clearStorage() {
+        localStorage = null;
+        File toRemove = new File("src/test/resources/test_pod/");
+
+        String[] entries = toRemove.list();
+
+        for(String s: entries){
+            File currentFile = new File(toRemove.getPath(), s);
+            currentFile.delete();
+        }
+
+        toRemove.delete();
+    }
+
+    @Test
+    public void test_create_positive() throws IOException {
+        Assertions.assertTrue(localStorage.store("A very nice message!"));
+        Assertions.assertTrue(localStorage.store("Second message!"));
+    }
+
+    @Test
+    public void test_exists() throws IOException {
+        Assertions.assertTrue(localStorage.store("A very nice message!"));
+        Assertions.assertTrue(localStorage.store("Extra message"));
+        Assertions.assertTrue(localStorage.exists("A very nice message!"));
+        Assertions.assertFalse(localStorage.exists("Doesn't exist"));
+    }
+
+    @Test
+    public void test_create_negative() throws IOException {
+        Assertions.assertTrue(localStorage.store("A very nice message!"));
+        Assertions.assertFalse(localStorage.store("A very nice message!"));
+        Assertions.assertTrue(localStorage.store("Second message!"));
+        Assertions.assertFalse(localStorage.store("Second message!"));
+    }
+
+    @Test
+    public void test_remove() throws IOException {
+        Assertions.assertTrue(localStorage.store("A very nice message!"));
+        Assertions.assertTrue(localStorage.exists("A very nice message!"));
+        Assertions.assertTrue(localStorage.delete("A very nice message!"));
+        Assertions.assertFalse(localStorage.exists("A very nice message!"));
+    }
+
+    @Test
+    public void test_remove_negative() throws IOException {
+        Assertions.assertFalse(localStorage.delete("Non existent message"));
+    }
+
+
+    //From: https://stackoverflow.com/questions/19600527/java-program-setting-an-environment-variable
+    public static void setEnv(String key, String value) {
+        try {
+            Map<String, String> env = System.getenv();
+            Class<?> cl = env.getClass();
+            Field field = cl.getDeclaredField("m");
+            field.setAccessible(true);
+            Map<String, String> writableEnv = (Map<String, String>) field.get(env);
+            writableEnv.put(key, value);
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to set environment variable", e);
+        }
+    }
+}
