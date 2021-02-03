2e79792e81c5b4fcc47abc61acca4667c26838c8
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2021-02-03 01:04:44 +0100

    Added logging

--- core/src/main/java/eu/fasten/core/search/SearchEngine.java
+++ core/src/main/java/eu/fasten/core/search/SearchEngine.java
@@ -33,6 +33,9 @@ import org.jooq.Record2;
 import org.jooq.conf.ParseUnknownFunctions;
 import org.rocksdb.RocksDBException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPResult;
 import com.martiansoftware.jsap.Parameter;
@@ -64,6 +67,7 @@ import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
  */
 
 public class SearchEngine {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SearchEngine.class);
 
 	public final static class Result {
 		public long gid;
@@ -150,10 +154,14 @@ public class SearchEngine {
 		final String version = record.component2();
 		final Set<Revision> dependencySet = resolver.resolveDependencies(groupId, artifactId, version, -1, context, true);
 
+		LOGGER.debug("Found " + dependencySet.size() + " dependencies");
+
 		final DatabaseMerger dm = new DatabaseMerger(LongOpenHashSet.toSet(dependencySet.stream().mapToLong(x -> x.id)), context, rocksDao);
 		final var stitchedGraph = dm.mergeWithCHA(groupId + ":" + artifactId + ":" + version);
 
 		if (!stitchedGraph.nodes().contains(gid)) throw new IllegalStateException("The stitched graph does not contain the given callable");
+
+		LOGGER.debug("Stiched graph has " + stitchedGraph.numNodes() + " nodes");
 
 		final ArrayList<Result> results = new ArrayList<>();
 
@@ -161,6 +169,8 @@ public class SearchEngine {
 		reachable.forEachRemaining((x) -> {
 			if (filter.test(x)) results.add(new Result(x, (stitchedGraph.outdegree(x) + stitchedGraph.indegree(x)) / reachable.getShortestPathLength(x)));
 		});
+
+		LOGGER.debug("Found " + results.size() + " reachable nodes");
 
 		Collections.sort(results, (x, y) -> Double.compare(y.score, x.score));
 		return results;
@@ -179,7 +189,7 @@ public class SearchEngine {
 		final long rev = gid2Rev(gid);
 
 		final var graph = rocksDao.getGraphData(rev);
-		if (graph == null) throw new NoSuchElementException("Revision associated with callable missing fromå the graph database");
+		if (graph == null) throw new NoSuchElementException("Revision associated with callable missing from the graph database");
 
 		Record2<String, String> record = context.select(Packages.PACKAGES.PACKAGE_NAME, PackageVersions.PACKAGE_VERSIONS.VERSION).from(PackageVersions.PACKAGE_VERSIONS).join(Packages.PACKAGES).on(PackageVersions.PACKAGE_VERSIONS.PACKAGE_ID.eq(Packages.PACKAGES.ID)).where(PackageVersions.PACKAGE_VERSIONS.ID.eq(rev)).fetchOne();
 		String[] a = record.component1().split(":");
@@ -188,32 +198,47 @@ public class SearchEngine {
 		String version = record.component2();
 		final Set<Revision> dependentSet = resolver.resolveDependents(groupId, artifactId, version, -1, true);
 
+		LOGGER.debug("Found " + dependentSet.size() + " dependents");
+
 		final LongOpenHashSet dependentIds = LongOpenHashSet.toSet(dependentSet.stream().mapToLong(x -> x.id));
 		dependentIds.add(rev);
+		LOGGER.debug("Found " + dependentIds.size() + " dependents");
 
 		final ArrayList<Result> results = new ArrayList<>();
 
 		for (final var dependentId : dependentIds) {
 			record = context.select(Packages.PACKAGES.PACKAGE_NAME, PackageVersions.PACKAGE_VERSIONS.VERSION).from(PackageVersions.PACKAGE_VERSIONS).join(Packages.PACKAGES).on(PackageVersions.PACKAGE_VERSIONS.PACKAGE_ID.eq(Packages.PACKAGES.ID)).where(PackageVersions.PACKAGE_VERSIONS.ID.eq(dependentId)).fetchOne();
+
+			LOGGER.debug("Analyzing dependent " + groupId + ":" + artifactId + ":" + version);
 			a = record.component1().split(":");
 			groupId = a[0];
 			artifactId = a[1];
 			version = record.component2();
 			final Set<Revision> dependencySet = resolver.resolveDependencies(groupId, artifactId, version, -1, context, true);
 
+			LOGGER.debug("Found " + dependencySet.size() + " dependencies");
+
 			final LongOpenHashSet dependencyIds = LongOpenHashSet.toSet(dependencySet.stream().mapToLong(x -> x.id));
-			if (!dependencyIds.contains(rev)) continue; // We cannot possibly reach the callable
+			if (dependentId != rev && !dependencyIds.contains(dependentId)) {
+				LOGGER.debug("False dependent");
+				continue; // We cannot possibly reach the callable
+			}
 			final DatabaseMerger dm = new DatabaseMerger(dependencyIds, context, rocksDao);
 			final var stitchedGraph = dm.mergeWithCHA(groupId + ":" + artifactId + ":" + version);
 			if (!stitchedGraph.nodes().contains(gid)) continue; // We cannot possibly reach the callable
+
+			LOGGER.debug("Stiched graph has " + stitchedGraph.numNodes() + " nodes");
+			final int sizeBefore = results.size();
 
 			final ClosestFirstIterator<Long, LongLongPair> coreachable = new ClosestFirstIterator<>(new EdgeReversedGraph<>(stitchedGraph), Long.valueOf(gid));
 			coreachable.forEachRemaining((x) -> {
 				if (filter.test(x)) results.add(new Result(x, (stitchedGraph.outdegree(x) + stitchedGraph.indegree(x)) / coreachable.getShortestPathLength(x)));
 			});
 
+			LOGGER.debug("Found " + (results.size() - sizeBefore) + " coreachable nodes");
 		}
 
+		LOGGER.debug("Found overall " + results.size() + " coreachable nodes");
 		Collections.sort(results, (x, y) -> Double.compare(y.score, x.score));
 		return results;
 	}
@@ -241,13 +266,18 @@ public class SearchEngine {
 
 		final Scanner scanner = new Scanner(System.in);
 		while (scanner.hasNextLine()) {
+			System.out.print(">");
+			System.out.flush();
 			String line = scanner.nextLine();
 			try {
 				final char dir = line.charAt(0);
 				line = line.substring(1);
 				final FastenJavaURI uri = FastenJavaURI.create(line);
 				final long gid = Util.getCallableGID(uri, searchEngine.context);
-
+				if (gid == -1) {
+					System.err.println("Unknown URI " + uri);
+					continue;
+				}
 				final var r = dir == '+' ? searchEngine.fromCallable(gid, x -> true) : searchEngine.toCallable(gid, x -> true);
 				for(int i = 0; i < Math.min(10, r.size()); i++)
 					System.out.println(r.get(i).gid + "\t" + Util.getCallableName(r.get(i).gid, searchEngine.context) + "\t" + r.get(i).score);
