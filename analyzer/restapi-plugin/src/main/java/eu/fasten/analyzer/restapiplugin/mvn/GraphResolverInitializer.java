1001e75ac0a45d2126b7f33b767d9ecee9ef3364
MihhailSokolov <m.sokolov@student.tudelft.nl>
2021-03-15 10:31:12 +0100

    Fix lazy graph resolver initialization

--- analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/GraphResolverInitializer.java
+++ analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/GraphResolverInitializer.java
@@ -7,7 +7,7 @@ public class GraphResolverInitializer {
 
     public static void sendRequestToInitGraphResolver() {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8080/mvn/packages/__INTERNAL__/packages/0/directedgraph";
+        String url = "http://localhost:8080/__INTERNAL__/packages/0/directedgraph";
         try {
             restTemplate.getForObject(url, String.class);
         } catch (RestClientException ignored) {}
