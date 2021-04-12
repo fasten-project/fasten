f148f35057c1aa507b754e9fa42260676f5e0601
Marco Micera <marco.micera@endocode.com>
2021-04-12 18:21:49 +0200

    Pom file retriever; XML check; tests

--- analyzer/license-detector/src/main/java/eu/fasten/analyzer/licensedetector/LicenseDetectorPlugin.java
+++ analyzer/license-detector/src/main/java/eu/fasten/analyzer/licensedetector/LicenseDetectorPlugin.java
@@ -7,10 +7,17 @@ import org.pf4j.Plugin;
 import org.pf4j.PluginWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 
 public class LicenseDetectorPlugin extends Plugin {
@@ -29,16 +36,21 @@ public class LicenseDetectorPlugin extends Plugin {
         /**
          * The topic this plugin consumes.
          */
-        protected String inputTopic = "fasten.RepoCloner.out";
+        protected String consumerTopic = "fasten.RepoCloner.out";
+
+        /**
+         * Patch to be applied to pom.xml files before the analysis begins.
+         */
+        protected static final String POM_PATCH_FILE = "pom-patch.xml";
 
         @Override
         public Optional<List<String>> consumeTopic() {
-            return Optional.of(Collections.singletonList(inputTopic));
+            return Optional.of(Collections.singletonList(consumerTopic));
         }
 
         @Override
         public void setTopic(String topicName) {
-            this.inputTopic = topicName;
+            this.consumerTopic = topicName;
         }
 
         @Override
@@ -53,11 +65,12 @@ public class LicenseDetectorPlugin extends Plugin {
                 String repoPath = extractRepoPath(record);
                 logger.info("License detector: scanning repository in " + repoPath + "...");
 
+                patchPomFile(repoPath);
+
             } catch (Exception e) { // Fasten error-handling guidelines
                 logger.error(e.getMessage());
                 setPluginError(e);
             }
-
         }
 
         /**
@@ -78,6 +91,93 @@ public class LicenseDetectorPlugin extends Plugin {
             }
             return repoPath;
         }
+
+        /**
+         * Retrieves the pom.xml file given a repository path.
+         *
+         * @param repoPath the repository path whose pom.xml file must be retrieved.
+         * @return the pom.xml file of the repository.
+         */
+        protected Optional<File> retrievePomFile(String repoPath) {
+
+            // Result
+            Optional<File> pomFile = Optional.empty();
+
+            // Repository folder
+            File repoFolder = new File(repoPath);
+
+            // Retrieving all repository's pom files
+            File[] pomFiles = repoFolder.listFiles((dir, name) -> name.equalsIgnoreCase("pom.xml"));
+            if (pomFiles.length == 0) {
+                logger.error("No pom.xml file found in " + repoFolder.getAbsolutePath() + ".");
+            } else if (pomFiles.length == 1) {
+                pomFile = Optional.ofNullable(pomFiles[0]);
+            } else {
+                // Retrieving the pom.xml file having the shortest path (closest to it repository's root path)
+                pomFile = Arrays.stream(pomFiles).min(Comparator.comparingInt(f -> f.getAbsolutePath().length()));
+                logger.info("Multiple pom.xml files found. Using " + pomFile.get());
+            }
+
+            return pomFile;
+        }
+
+        protected File patchPomFile(String repoPath) throws IllegalArgumentException, ParserConfigurationException {
+
+            // Retrieving the pom file
+            Optional<File> pomFile = retrievePomFile(repoPath);
+            if (pomFile.isEmpty()) {
+                throw new IllegalArgumentException("No file named pom.xml found in " + repoPath + ". " +
+                        "This plugin only analyzes Maven projects.");
+            }
+
+            // Retrieving the patch XML file
+            File patchFile = new File(Objects.requireNonNull(LicenseDetector.class.getClassLoader()
+                    .getResource(POM_PATCH_FILE)).getFile());
+
+            // Checking whether these two XML files are well-formed
+            if (xmlFileIsWellFormed(pomFile.get())) {
+                throw new IllegalArgumentException("Repository's pom.xml is malformed.");
+            }
+            if (xmlFileIsWellFormed(patchFile)) {
+                throw new IllegalArgumentException("Path file is malformed.");
+            }
+
+            return null; // FIXME
+        }
+
+        protected boolean xmlFileIsWellFormed(File xmlFile) throws ParserConfigurationException {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            builder.setErrorHandler(new ErrorHandler() {
+                @Override
+                public void warning(SAXParseException exception) {
+                    logger.warn(exception.getMessage());
+                }
+
+                @Override
+                public void error(SAXParseException exception) {
+                    logger.error(exception.getMessage());
+                }
+
+                @Override
+                public void fatalError(SAXParseException exception) {
+                    logger.error("Fatal: " + exception.getMessage());
+                }
+            });
+
+            try {
+                Document parse = builder.parse(xmlFile);
+            } catch (SAXException e) {
+                logger.warn("XML file " + xmlFile.getAbsolutePath() + " is malformed.");
+                return true;
+            } catch (IOException e) {
+                throw new IllegalArgumentException("File " + xmlFile.getAbsolutePath() + " not found.");
+            }
+
+            return false;
+        }
+
 
         @Override
         public Optional<String> produce() {
@@ -136,7 +236,7 @@ public class LicenseDetectorPlugin extends Plugin {
 
         @Override
         public long getMaxConsumeTimeout() {
-            return 1 * 60 * 60 * 1000; // 1 hour
+            return 1 * 60 * 60 * 1000; // FIXME 1 hour
         }
     }
 }
