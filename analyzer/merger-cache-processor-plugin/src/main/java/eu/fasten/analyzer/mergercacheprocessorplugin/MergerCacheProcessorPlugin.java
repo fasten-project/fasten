f0305f4c88208cdd87fbbeb55330accb1bd594b7
romatallinn <sirokov@dizconto.com>
2021-05-06 18:04:31 +0200

    Add backbone module for the cache processor plugin

---
+++ analyzer/merger-cache-processor-plugin/src/main/java/eu/fasten/analyzer/mergercacheprocessorplugin/MergerCacheProcessorPlugin.java
@@ -0,0 +1,11 @@
+package eu.fasten.analyzer.mergercacheprocessorplugin;
+
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+public class MergerCacheProcessorPlugin extends Plugin {
+    public MergerCacheProcessorPlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+}
+
