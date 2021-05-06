ead12c8f77bf16de68830cbca834cd351fbe2f08
romatallinn <sirokov@dizconto.com>
2021-05-06 17:59:17 +0200

    Add backbone module for the cache invalidator plugin

---
+++ analyzer/merger-cache-invalidation-plugin/src/main/java/eu/fasten/analyzer/mergercacheinvalidationplugin/MergerCacheInvalidationPlugin.java
@@ -0,0 +1,11 @@
+package eu.fasten.analyzer.mergercacheinvalidationplugin;
+
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+public class MergerCacheInvalidationPlugin extends Plugin {
+    public MergerCacheInvalidationPlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+}
+
