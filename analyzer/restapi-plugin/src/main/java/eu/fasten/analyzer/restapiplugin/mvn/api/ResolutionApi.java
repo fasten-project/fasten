469403bf46fbd3c4469bad2455fc206802a86198
MihhailSokolov <m.sokolov@student.tudelft.nl>
2021-05-26 18:48:30 +0300

    Minor style fix

--- analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/api/ResolutionApi.java
+++ analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/api/ResolutionApi.java
@@ -21,7 +21,12 @@ package eu.fasten.analyzer.restapiplugin.mvn.api;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Lazy
