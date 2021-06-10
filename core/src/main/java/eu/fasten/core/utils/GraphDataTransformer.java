d5e7df5312ec2f87e705e28429dfdc56bf753908
MihhailSokolov <m.sokolov@student.tudelft.nl>
2021-06-10 16:10:02 +0300

    Small fix

--- core/src/main/java/eu/fasten/core/utils/GraphDataTransformer.java
+++ core/src/main/java/eu/fasten/core/utils/GraphDataTransformer.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package eu.fasten.core.utils;
 
 import eu.fasten.core.data.DirectedGraph;
@@ -5,11 +23,8 @@ import eu.fasten.core.data.graphdb.ExtendedGidGraph;
 import eu.fasten.core.data.graphdb.RocksDao;
 import eu.fasten.core.data.metadatadb.codegen.tables.*;
 import eu.fasten.core.data.metadatadb.codegen.tables.records.CallablesRecord;
-import eu.fasten.core.data.metadatadb.codegen.tables.records.ModulesRecord;
 import eu.fasten.core.dbconnectors.PostgresConnector;
-import eu.fasten.core.merge.LocalMerger;
 import org.apache.commons.math3.util.Pair;
-import org.apache.kafka.common.protocol.types.Field;
 import org.jooq.DSLContext;
 import org.rocksdb.RocksDBException;
 import org.slf4j.Logger;
