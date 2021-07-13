eea590e5b83a0894c5e4c00de554ef773ceb5f0d
MihhailSokolov <m.sokolov@student.tudelft.nl>
2021-07-13 15:41:16 +0300

    Add static and dynamic CGs combiner

---
+++ core/src/main/java/eu/fasten/core/dynamic/StaticDynamicCGCombiner.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.fasten.core.dynamic;
+
+import eu.fasten.core.data.ArrayImmutableDirectedGraph;
+import eu.fasten.core.data.DirectedGraph;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.longs.LongLongPair;
+import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectObjectImmutablePair;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class StaticDynamicCGCombiner {
+
+    private final Long2ObjectMap<String> staticCgUriMap;
+    private final Set<LongLongPair> staticCalls;
+    private final Long2ObjectMap<String> dynamicCgUriMap;
+    private final Set<LongLongPair> dynamicCalls;
+    private Long2ObjectMap<String> allUrisMap;
+
+    public StaticDynamicCGCombiner(final DirectedGraph staticCg,
+                                   final Map<Long, String> staticCgUriMap,
+                                   final DynamicJavaCG dynamicCg) {
+        this.staticCgUriMap = new Long2ObjectOpenHashMap<>(staticCgUriMap);
+        this.staticCalls = staticCg.edgeSet();
+        this.dynamicCgUriMap = dynamicCg.getMethods();
+        this.dynamicCalls = dynamicCg.getCalls();
+        this.allUrisMap = null;
+    }
+
+    public Long2ObjectMap<String> getAllUrisMap() {
+        return this.allUrisMap;
+    }
+
+    public DirectedGraph combineCGs() {
+        var staticUriCalls = new ObjectOpenHashSet<ObjectObjectImmutablePair<String, String>>(this.staticCalls.size());
+        this.staticCalls.forEach(call -> staticUriCalls.add(ObjectObjectImmutablePair.of(
+                this.staticCgUriMap.get(call.firstLong()),
+                this.staticCgUriMap.get(call.secondLong())
+        )));
+        var dynamicUriCalls = new ObjectOpenHashSet<ObjectObjectImmutablePair<String, String>>(this.dynamicCalls.size());
+        this.dynamicCalls.forEach(call -> dynamicUriCalls.add(ObjectObjectImmutablePair.of(
+                this.dynamicCgUriMap.get(call.firstLong()),
+                this.dynamicCgUriMap.get(call.secondLong())
+        )));
+        var allUriCalls = new ObjectOpenHashSet<ObjectObjectImmutablePair<String, String>>(this.staticCalls.size() + this.dynamicCalls.size());
+        allUriCalls.addAll(staticUriCalls);
+        allUriCalls.addAll(dynamicUriCalls);
+        var allUrisMap = new Long2ObjectOpenHashMap<String>(this.staticCgUriMap.size() + this.dynamicCgUriMap.size());
+        var c = 0L;
+        for (var call : allUriCalls) {
+            var source = call.left();
+            var target = call.right();
+            if (!allUrisMap.containsValue(source)) {
+                allUrisMap.put(c++, source);
+            }
+            if (!allUrisMap.containsValue(target)) {
+                allUrisMap.put(c++, target);
+            }
+        }
+        this.allUrisMap = allUrisMap;
+        var inverseUrisMap = new Object2LongOpenHashMap<String>(allUrisMap.size());
+        allUrisMap.forEach((k, v) -> inverseUrisMap.put(v, k.longValue()));
+        var graphBuilder = new ArrayImmutableDirectedGraph.Builder();
+        allUrisMap.keySet().forEach(graphBuilder::addInternalNode);
+        allUriCalls.forEach(call -> graphBuilder.addArc(
+                inverseUrisMap.getLong(call.left()),
+                inverseUrisMap.getLong(call.right())
+        ));
+        return graphBuilder.build();
+    }
+}
---
+++ core/src/test/java/eu/fasten/core/dynamic/StaticDynamicCGCombinerTest.java
@@ -0,0 +1,72 @@
+package eu.fasten.core.dynamic;
+
+import eu.fasten.core.data.ArrayImmutableDirectedGraph;
+import it.unimi.dsi.fastutil.Pair;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.longs.LongLongImmutablePair;
+import it.unimi.dsi.fastutil.longs.LongLongPair;
+import it.unimi.dsi.fastutil.objects.ObjectObjectImmutablePair;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import java.util.Set;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StaticDynamicCGCombinerTest {
+
+    @Test
+    public void combineCGsTest() {
+        var graphBuilder = new ArrayImmutableDirectedGraph.Builder();
+        graphBuilder.addInternalNode(0);
+        graphBuilder.addInternalNode(1);
+        graphBuilder.addInternalNode(2);
+        graphBuilder.addInternalNode(3);
+        graphBuilder.addArc(1, 2);
+        graphBuilder.addArc(0, 3);
+        var staticCg = graphBuilder.build();
+        var staticCgUriMap = Map.of(
+                0L, "/name.space/SingleSourceToTarget.SingleSourceToTarget()%2Fjava.lang%2FVoidType",
+                1L, "/name.space/SingleSourceToTarget.sourceMethod()%2Fjava.lang%2FVoidType",
+                2L, "/name.space/SingleSourceToTarget.targetMethod()%2Fjava.lang%2FVoidType",
+                3L, "/java.lang/Object.Object()VoidType"
+        );
+        var dynamicMethods = new Long2ObjectOpenHashMap<String>(3);
+        dynamicMethods.put(3L, "/java.lang/Object.Object()VoidType");
+        dynamicMethods.put(1L, "/name.space/SingleSourceToTarget.sourceMethod()%2Fjava.lang%2FVoidType");
+        dynamicMethods.put(2L, "/name.space/SingleSourceToTarget.targetMethod()%2Fjava.lang%2FVoidType");
+        var dynamicCalls = new ObjectOpenHashSet<LongLongPair>();
+        dynamicCalls.add(LongLongImmutablePair.of(1, 2));
+        dynamicCalls.add(LongLongImmutablePair.of(2, 3));
+        var dynamicCg = new DynamicJavaCG(dynamicMethods, dynamicCalls);
+
+        var combiner = new StaticDynamicCGCombiner(staticCg, staticCgUriMap, dynamicCg);
+        var result = combiner.combineCGs();
+        var uris = combiner.getAllUrisMap();
+
+        var expectedMethods = Set.of(
+                "/java.lang/Object.Object()VoidType",
+                "/name.space/SingleSourceToTarget.SingleSourceToTarget()%2Fjava.lang%2FVoidType",
+                "/name.space/SingleSourceToTarget.sourceMethod()%2Fjava.lang%2FVoidType",
+                "/name.space/SingleSourceToTarget.targetMethod()%2Fjava.lang%2FVoidType"
+        );
+        var expectedCalls = Set.of(
+                Pair.of("/name.space/SingleSourceToTarget.sourceMethod()%2Fjava.lang%2FVoidType",
+                        "/name.space/SingleSourceToTarget.targetMethod()%2Fjava.lang%2FVoidType"),
+                Pair.of("/name.space/SingleSourceToTarget.SingleSourceToTarget()%2Fjava.lang%2FVoidType",
+                        "/java.lang/Object.Object()VoidType"),
+                Pair.of("/name.space/SingleSourceToTarget.targetMethod()%2Fjava.lang%2FVoidType",
+                        "/java.lang/Object.Object()VoidType")
+        );
+        
+        var resultMethods = new ObjectOpenHashSet<String>(result.numNodes());
+        result.nodes().forEach(n -> resultMethods.add(uris.get(n)));
+        assertEquals(expectedMethods, resultMethods);
+
+        var resultCalls = new ObjectOpenHashSet<ObjectObjectImmutablePair<String, String>>(result.edgeSet().size());
+        result.edgeSet().forEach(e -> resultCalls.add(ObjectObjectImmutablePair.of(
+                uris.get(e.firstLong()),
+                uris.get(e.secondLong())
+        )));
+        assertEquals(expectedCalls, resultCalls);
+    }
+}
