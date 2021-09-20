7c8a2553193b2208b376b53329da6c0467abc71a
ashkboos <m.keshani@tudelft.nl>
2021-09-20 11:13:26 +0200

    check individual artifact in graph checker

--- core/src/main/java/eu/fasten/core/data/graphdb/utils/GraphDBChecker.java
+++ core/src/main/java/eu/fasten/core/data/graphdb/utils/GraphDBChecker.java
@@ -20,8 +20,11 @@ package eu.fasten.core.data.graphdb.utils;
 
 import eu.fasten.core.data.graphdb.RocksDao;
 import eu.fasten.core.data.metadatadb.codegen.tables.PackageVersions;
+import eu.fasten.core.data.utils.DirectedGraphSerializer;
 import eu.fasten.core.dbconnectors.PostgresConnector;
 import eu.fasten.core.dbconnectors.RocksDBConnector;
+import eu.fasten.core.merge.CallGraphUtils;
+import java.io.IOException;
 import org.jooq.Record1;
 import org.rocksdb.RocksDBException;
 import org.slf4j.Logger;
@@ -60,6 +63,16 @@ public class GraphDBChecker implements Runnable {
             defaultValue = "fastenro")
     String metadataDbUser;
 
+    @CommandLine.Option(names = {"-a", "--artifactId"},
+        paramLabel = "ARTIFACT",
+        description = "The artifact id to check in the graph database")
+    String artifactId;
+
+    @CommandLine.Option(names = {"-o", "--outputPath"},
+        paramLabel = "OUTPUT",
+        description = "The path to the directory to write the ")
+    String outDir;
+
     public static void main(String[] args) {
         final int exitCode = new CommandLine(new GraphDBChecker()).execute(args);
         System.exit(exitCode);
@@ -67,18 +80,64 @@ public class GraphDBChecker implements Runnable {
 
     @Override
     public void run() {
-        List<Long> packageVersionIds;
-        if (!noDb) {
+        if (artifactId != null & outDir != null) {
+            writeArtifact(graphDbPath, artifactId, outDir);
+        } else {
+            List<Long> packageVersionIds;
+            if (!noDb) {
+                try {
+                    var metadataDb =
+                        PostgresConnector.getDSLContext(metadataDbUrl, metadataDbUser, true);
+                    packageVersionIds = metadataDb.select(PackageVersions.PACKAGE_VERSIONS.ID)
+                        .from(PackageVersions.PACKAGE_VERSIONS).fetch().map(Record1::value1);
+                } catch (Exception e) {
+                    logger.error("Error connecting to the metadata database", e);
+                    return;
+                }
+            } else {
+                packageVersionIds =
+                    LongStream.rangeClosed(0L, 10000000L).boxed().collect(Collectors.toList());
+            }
+            RocksDao rocksDb;
             try {
-                var metadataDb = PostgresConnector.getDSLContext(metadataDbUrl, metadataDbUser, true);
-                packageVersionIds = metadataDb.select(PackageVersions.PACKAGE_VERSIONS.ID).from(PackageVersions.PACKAGE_VERSIONS).fetch().map(Record1::value1);
+                rocksDb = RocksDBConnector.createReadOnlyRocksDBAccessObject(graphDbPath);
             } catch (Exception e) {
-                logger.error("Error connecting to the metadata database", e);
+                logger.error("Error connecting to the graph database", e);
                 return;
             }
-        } else {
-            packageVersionIds = LongStream.rangeClosed(0L, 10000000L).boxed().collect(Collectors.toList());
+            logger.info("Connected to both databases");
+            logger.info("Retrieved package versions' IDs ({} in total)", packageVersionIds.size());
+            var successfulDirectedGraph = 0;
+            var successfulGraphMetadata = 0;
+            for (var packageVersionId : packageVersionIds) {
+                try {
+                    logger.info("Retrieving data for package version ID {}", packageVersionId);
+                    var graph = rocksDb.getGraphData(packageVersionId);
+                    if (graph != null) {
+                        successfulDirectedGraph++;
+                        var graphMetadata = rocksDb.getGraphMetadata(packageVersionId, graph);
+                        if (graphMetadata != null) {
+                            successfulGraphMetadata++;
+                        }
+                    }
+                } catch (RocksDBException ignored) {
+                }
+            }
+            logger.info("Finished");
+            if (!noDb) {
+                logger.info("Graph database contains {} out of {} directed graphs",
+                    successfulDirectedGraph, packageVersionIds.size());
+                logger.info("Graph database contains {} out of {} graph metadata objects",
+                    successfulGraphMetadata, packageVersionIds.size());
+            } else {
+                logger.info("Graph database contains {} directed graphs", successfulDirectedGraph);
+                logger.info("Graph database contains {} graph metadata objects",
+                    successfulGraphMetadata);
+            }
         }
+    }
+
+    private void writeArtifact(String graphDbPath, String artifactId, String outDir) {
         RocksDao rocksDb;
         try {
             rocksDb = RocksDBConnector.createReadOnlyRocksDBAccessObject(graphDbPath);
@@ -86,30 +145,25 @@ public class GraphDBChecker implements Runnable {
             logger.error("Error connecting to the graph database", e);
             return;
         }
-        logger.info("Connected to both databases");
-        logger.info("Retrieved package versions' IDs ({} in total)", packageVersionIds.size());
-        var successfulDirectedGraph = 0;
-        var successfulGraphMetadata = 0;
-        for (var packageVersionId : packageVersionIds) {
-            try {
-                logger.info("Retrieving data for package version ID {}", packageVersionId);
-                var graph = rocksDb.getGraphData(packageVersionId);
-                if (graph != null) {
-                    successfulDirectedGraph++;
-                    var graphMetadata = rocksDb.getGraphMetadata(packageVersionId, graph);
-                    if (graphMetadata != null) {
-                        successfulGraphMetadata++;
-                    }
+        logger.info("Connected to database");
+        try {
+            logger.info("Retrieving data for package version ID {}", artifactId);
+            long packageVersionId = Long.parseLong(artifactId);
+            var graph = rocksDb.getGraphData(packageVersionId);
+            if (graph != null) {
+                CallGraphUtils.writeToFile(outDir+"+/"+ artifactId+".directedGraph.txt",
+                    graph.toString(), "");
+                var graphMetadata = rocksDb.getGraphMetadata(packageVersionId, graph);
+                if (graphMetadata != null) {
+                    CallGraphUtils.writeToFile(outDir+"+/"+ artifactId+".graphMetadata.txt",
+                        graphMetadata.toString(), "");
+                }else {
+                    logger.info("No metadata for the artifact {}", artifactId);
                 }
-            } catch (RocksDBException ignored) {}
-        }
-        logger.info("Finished");
-        if (!noDb) {
-            logger.info("Graph database contains {} out of {} directed graphs", successfulDirectedGraph, packageVersionIds.size());
-            logger.info("Graph database contains {} out of {} graph metadata objects", successfulGraphMetadata, packageVersionIds.size());
-        } else {
-            logger.info("Graph database contains {} directed graphs", successfulDirectedGraph);
-            logger.info("Graph database contains {} graph metadata objects", successfulGraphMetadata);
+            }else {
+                logger.info("No graph for this artifact {}", artifactId);
+            }
+        } catch (RocksDBException | IOException ignored) {
         }
     }
 }
