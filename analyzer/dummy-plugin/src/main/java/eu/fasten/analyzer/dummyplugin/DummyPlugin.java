33967070cf869dca7b41ac89d602cba281d43ef7
Amir M. Mir <mir-am@hotmail.com>
2021-10-14 10:16:52 +0200

    A dummy plug-in to test the FASTEN server w/ normal/prio messages

---
+++ analyzer/dummy-plugin/src/main/java/eu/fasten/analyzer/dummyplugin/DummyPlugin.java
@@ -0,0 +1,82 @@
+package eu.fasten.analyzer.dummyplugin;
+import eu.fasten.core.plugins.KafkaPlugin;
+import org.pf4j.Extension;
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DummyPlugin extends Plugin {
+    public DummyPlugin (PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Extension
+    public static class DummyPluginExtension implements KafkaPlugin {
+
+        private final Logger logger = LoggerFactory.getLogger(DummyPlugin.class.getName());
+
+        @Override
+        public Optional<List<String>> consumeTopic() {
+            return Optional.empty();
+        }
+
+        @Override
+        public void setTopics(List<String> consumeTopics) {
+
+        }
+
+        @Override
+        public void consume(String record) {
+            logger.info("Processing" + record);
+        }
+
+        @Override
+        public Optional<String> produce() {
+            return Optional.empty();
+        }
+
+        @Override
+        public String getOutputPath() {
+            return null;
+        }
+
+        @Override
+        public String name() {
+            return null;
+        }
+
+        @Override
+        public String description() {
+            return null;
+        }
+
+        @Override
+        public String version() {
+            return null;
+        }
+
+        @Override
+        public void start() {
+
+        }
+
+        @Override
+        public void stop() {
+
+        }
+
+        @Override
+        public Exception getPluginError() {
+            return null;
+        }
+
+        @Override
+        public void freeResource() {
+
+        }
+    }
+}
