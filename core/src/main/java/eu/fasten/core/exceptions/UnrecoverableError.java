34325e9488e74b4dce62a21a895efb1436156db9
Amir M. Mir <mir-am@hotmail.com>
2021-11-22 15:26:41 +0100

    UnrecoverableError now extends Error class, not RTE

--- core/src/main/java/eu/fasten/core/exceptions/UnrecoverableError.java
+++ core/src/main/java/eu/fasten/core/exceptions/UnrecoverableError.java
@@ -4,7 +4,7 @@ package eu.fasten.core.exceptions;
  * This exception is defined for rare circumstances where we need to deliberately crash a plug-in and
  * cause Kubernetes to restart the pod. Otherwise, RuntimeExceptions are normally catched and handled by the FASTEN server.
  */
-public class UnrecoverableError extends RuntimeException {
+public class UnrecoverableError extends Error {
     public UnrecoverableError(String errorMsg, Throwable err) {
         super(errorMsg, err);
     }
