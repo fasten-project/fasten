981355d51cb0aab48bb47bdee4343bbd4114476d
Sebastian Proksch <development@mail.proks.ch>
2021-11-23 23:54:02 +0100

    Added example projects that make playing around with OPAL easier

---
+++ analyzer/javacg-opal/src/test/resources/opal-examples/java-8-basic/src/main/java/constructors/NestedClassWithDefaultConstructor.java
@@ -0,0 +1,19 @@
+package constructors;
+
+public class NestedClassWithDefaultConstructor {
+
+	public class N {
+
+		public void entrypoint() {
+			// do something
+		}
+	}
+
+	public static class StaticN {
+
+		public void entrypoint() {
+			// do something
+		}
+	}
+
+}
