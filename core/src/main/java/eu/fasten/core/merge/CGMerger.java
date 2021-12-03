c4b056a20b52ba666e2e8e3913b3f48572373fd9
ashkboos <m.keshani@tudelft.nl>
2021-12-03 12:32:22 +0100

    fix the uri

--- core/src/main/java/eu/fasten/core/merge/CGMerger.java
+++ core/src/main/java/eu/fasten/core/merge/CGMerger.java
@@ -99,14 +99,17 @@ public class CGMerger {
         }
         BiMap<Long, String> uris = HashBiMap.create();
         dbContext
-            .select(Callables.CALLABLES.ID, concat(Callables.CALLABLES.ID,
-                Callables.CALLABLES.FASTEN_URI))
+            .select(Callables.CALLABLES.ID, Packages.PACKAGES.PACKAGE_NAME,
+                    PackageVersions.PACKAGE_VERSIONS.VERSION,
+                    Callables.CALLABLES.FASTEN_URI)
             .from(Callables.CALLABLES, Modules.MODULES, PackageVersions.PACKAGE_VERSIONS, Packages.PACKAGES)
             .where(Callables.CALLABLES.ID.in(gIDs))
             .and(Modules.MODULES.ID.eq(Callables.CALLABLES.MODULE_ID))
             .and(PackageVersions.PACKAGE_VERSIONS.ID.eq(Modules.MODULES.PACKAGE_VERSION_ID))
             .and(Packages.PACKAGES.ID.eq(PackageVersions.PACKAGE_VERSIONS.PACKAGE_ID))
-            .fetch().forEach(record -> uris.put(record.component1(), record.component2()));
+            .fetch().forEach(record -> uris.put( record.component1(),
+            "fasten://mvn!" + record.component2() + "$" + record.component3() + record.component4()));
+
         return uris;
     }
 
