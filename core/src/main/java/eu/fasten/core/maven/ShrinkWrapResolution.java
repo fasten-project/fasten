6f9c909f82ab71220d33cbd20ab63d95bf95765f
ashkboos <m.keshani@tudelft.nl>
2021-12-17 16:26:43 +0100

    do not show shrinkwrap logs

--- core/src/main/java/eu/fasten/core/maven/ShrinkWrapResolution.java
+++ core/src/main/java/eu/fasten/core/maven/ShrinkWrapResolution.java
@@ -1,7 +1,9 @@
 package eu.fasten.core.maven;
 
 import eu.fasten.core.maven.data.Revision;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.shrinkwrap.resolver.api.maven.coordinate.MavenCoordinate;
@@ -12,8 +14,13 @@ public class ShrinkWrapResolution implements ResolutionStrategy{
     public Set<Revision> resolve(Revision revision, boolean withTransitivity) {
 
         Set<Revision> result = new HashSet<>();
-        var depSet = Maven.resolver().resolve(revision.toCoordinate()).withTransitivity()
-            .asList(org.jboss.shrinkwrap.resolver.api.maven.coordinate.MavenCoordinate.class);
+        List<MavenCoordinate> depSet;
+        try {
+            depSet = Maven.resolver().resolve(revision.toCoordinate()).withTransitivity()
+                .asList(org.jboss.shrinkwrap.resolver.api.maven.coordinate.MavenCoordinate.class);
+        } catch (Exception e){
+            return result;
+        }
         if (depSet != null && !depSet.isEmpty()) {
             for (MavenCoordinate mavenCoordinate : depSet) {
                  result.add(new Revision(mavenCoordinate.getGroupId(),
