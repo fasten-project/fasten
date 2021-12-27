c791c45cb041e74fbb91084c12b5ee7b482ce0ca
Sebastian Proksch <development@mail.proks.ch>
2021-12-27 17:52:44 +0100

    Implement DatabaseUtils and integrate them in PomAnalyzerPlugin

--- analyzer/pom-analyzer/src/main/java/eu/fasten/analyzer/pomanalyzer/utils/DatabaseUtils.java
+++ analyzer/pom-analyzer/src/main/java/eu/fasten/analyzer/pomanalyzer/utils/DatabaseUtils.java
@@ -19,6 +19,7 @@ import static eu.fasten.core.maven.utils.MavenUtilities.MAVEN_CENTRAL_REPO;
 import static eu.fasten.core.utils.Asserts.assertNotNull;
 
 import java.sql.Timestamp;
+import java.util.Date;
 
 import org.jooq.DSLContext;
 import org.jooq.exception.DataAccessException;
@@ -33,13 +34,15 @@ import eu.fasten.core.data.metadatadb.MetadataDao;
 public class DatabaseUtils {
 
 	private DSLContext context;
-	private MetadataDao metadataDao;
 
 	private boolean processedRecord;
 
 	public void setDslContext(DSLContext context) {
 		this.context = context;
-		this.metadataDao = new MetadataDao(context);
+	}
+
+	protected MetadataDao getDao(DSLContext ctx) {
+		return new MetadataDao(ctx);
 	}
 
 	public void save(PomAnalysisResult result) {
@@ -48,9 +51,9 @@ public class DatabaseUtils {
 		while (!processedRecord && numTries < Constants.transactionRestartLimit) {
 			numTries++;
 			context.transaction(transaction -> {
-				metadataDao.setContext(DSL.using(transaction));
+				var dao = getDao(DSL.using(transaction));
 				try {
-					insertIntoDB(result);
+					insertIntoDB(result, dao);
 					processedRecord = true;
 				} catch (DataAccessException e) {
 					// Can be happen. Normally fixable through retrying.
@@ -58,52 +61,55 @@ public class DatabaseUtils {
 			});
 		}
 	}
-	
-	public boolean doesPackageExistInDb(String gapv) {
-		// gid:aid:packaging:version
-		return false;
-	}
 
-	public void insertIntoDB(PomAnalysisResult r) {
+	public void insertIntoDB(PomAnalysisResult r, MetadataDao dao) {
 		String product = r.groupId + Constants.mvnCoordinateSeparator + r.artifactId;
-		final var packageId = metadataDao.insertPackage(product, Constants.mvnForge, r.projectName, r.repoUrl, null);
+		final var packageId = dao.insertPackage(product, Constants.mvnForge, r.projectName, r.repoUrl, null);
 
 		var pvMeta = toJson(r);
-//		pvMeta.put("dependencyManagement",
-//				(r.dependencyManagement != null) ? new JSONArray(r.dependencyManagement) : null);
-//		pvMeta.put("dependencies",
-//				(r.dependencies != null) ? new JSONArray(r.dependencies) : null);
-//		pvMeta.put("commitTag", (r.commitTag != null) ? r.commitTag : "");
-//		pvMeta.put("sourcesUrl", (r.sourcesUrl != null) ? r.sourcesUrl : "");
-//		pvMeta.put("packagingType", (r.packagingType != null) ? r.packagingType : "");
-//		pvMeta.put("parentCoordinate", (r.parentCoordinate != null) ? r.parentCoordinate : "");
-
-		assertNotNull(r.artifactRepository);
 
 		var isMavenCentral = MAVEN_CENTRAL_REPO.equals(r.artifactRepository);
-		long artifactRepoId = isMavenCentral ? -1L : metadataDao.insertArtifactRepository(r.artifactRepository);
+		long artifactRepoId = isMavenCentral ? -1L : dao.insertArtifactRepository(r.artifactRepository);
 
 		// TODO: Why is the opalGenerator required here??
-		final var packageVersionId = metadataDao.insertPackageVersion(packageId, Constants.opalGenerator, r.version,
+		final var packageVersionId = dao.insertPackageVersion(packageId, Constants.opalGenerator, r.version,
 				artifactRepoId, null, getProperTimestamp(r.releaseDate), pvMeta);
 
 		for (var dep : r.dependencies) {
 			var depProduct = dep.groupId + Constants.mvnCoordinateSeparator + dep.artifactId;
-			final var depId = metadataDao.insertPackage(depProduct, Constants.mvnForge);
-			metadataDao.insertDependency(packageVersionId, depId, dep.getVersionConstraints(), null, null, null,
-					dep.toJSON());
+			final var depId = dao.insertPackage(depProduct, Constants.mvnForge);
+			dao.insertDependency(packageVersionId, depId, dep.getVersionConstraints(), null, null, null, dep.toJSON());
 		}
+	}
+
+	public void ingestPackage(PomAnalysisResult result) {
+		var dao = new MetadataDao(context);
+		var packageName = result.groupId + ":" + result.artifactId;
+		var time = new Timestamp(new Date().getTime());
+		dao.insertIngestedArtifact(packageName, result.version, time);
+	}
+
+	public boolean hasPackageBeenIngested(String gapv) {
+		// gid:aid:packaging:version
+		
+		String[] parts = gapv.split(":");
+		var depProduct = parts[0] + ":" + parts[1];
+		
+		var dao = new MetadataDao(context);
+		return dao.isArtifactIngested(depProduct, parts[3]);
 	}
 
 	private static Timestamp getProperTimestamp(long timestamp) {
 		if (timestamp == -1) {
 			return null;
 		} else {
+			// TODO get rid of this code if it does not appear in the log
 			if (timestamp / (1000L * 60 * 60 * 24 * 365) < 1L) {
-				return new Timestamp(timestamp * 1000);
-			} else {
-				return new Timestamp(timestamp);
+				// return new Timestamp(timestamp * 1000);
+				throw new RuntimeException(
+						"this should be a relict of the past, fix DatabaseUtils.getProperTimestamp, if this error appears in the log");
 			}
+			return new Timestamp(timestamp);
 		}
 	}
 
@@ -118,7 +124,6 @@ public class DatabaseUtils {
 		assertNotNull(d.packagingType);
 		assertNotNull(d.dependencies);
 		assertNotNull(d.dependencyManagement);
-		assertNotNull(d.sourcesUrl);
 		// TODO add remaining asserts
 
 		json.put("forge", d.forge);
@@ -134,13 +139,10 @@ public class DatabaseUtils {
 		json.put("dependencies", new JSONArray(d.dependencies).toString());
 		json.put("dependencyManagement", new JSONArray(d.dependencyManagement).toString());
 
-		json.put("resolvedCompileAndRuntimeDependencies",
-				new JSONArray(d.resolvedCompileAndRuntimeDependencies).toString());
-
 		json.put("date", d.releaseDate);
 		json.put("repoUrl", (d.repoUrl != null) ? d.repoUrl : "");
 		json.put("commitTag", (d.commitTag != null) ? d.commitTag : "");
-		json.put("sourcesUrl", d.sourcesUrl);
+		json.put("sourcesUrl", (d.sourcesUrl != null) ? d.sourcesUrl : "");
 		json.put("projectName", (d.projectName != null) ? d.projectName : "");
 
 		return json;
--- analyzer/pom-analyzer/src/test/java/eu/fasten/analyzer/pomanalyzer/utils/DatabaseUtilsTest.java
+++ analyzer/pom-analyzer/src/test/java/eu/fasten/analyzer/pomanalyzer/utils/DatabaseUtilsTest.java
@@ -15,14 +15,154 @@
  */
 package eu.fasten.analyzer.pomanalyzer.utils;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.jooq.DSLContext;
+import org.jooq.TransactionalRunnable;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import eu.fasten.analyzer.pomanalyzer.data.PomAnalysisResult;
+import eu.fasten.core.data.Constants;
+import eu.fasten.core.data.metadatadb.MetadataDao;
+import eu.fasten.core.maven.data.Dependency;
+import eu.fasten.core.maven.utils.MavenUtilities;
 
 public class DatabaseUtilsTest {
 
+	private MetadataDao dao;
+	private DSLContext dslContext;
+	private DatabaseUtils sut;
+
+	@BeforeEach
+	public void setup() {
+		dao = mock(MetadataDao.class, "MetadataDaoMock");
+		dslContext = mock(DSLContext.class, "DSLContextMock");
+		doAnswer(new Answer<Void>() {
+			public Void answer(InvocationOnMock i) throws Throwable {
+				var arg0 = (TransactionalRunnable) i.getArgument(0);
+				arg0.run(null);
+				return null;
+			};
+		}).when(dslContext).transaction(any(TransactionalRunnable.class));
+
+		sut = new DatabaseUtils() {
+			protected MetadataDao getDao(DSLContext ctx) {
+				return dao;
+			};
+		};
+		sut.setDslContext(dslContext);
+	}
+
 	@Test
-	public void todo() {
-		fail();
+	public void transactionIsStarted() {
+		var result = getSomeResult();
+		sut.save(result);
+		verify(dslContext).transaction(any(TransactionalRunnable.class));
+
+	}
+
+	@Test
+	public void storePackage() {
+		var result = getSomeResult();
+		result.artifactRepository = MavenUtilities.MAVEN_CENTRAL_REPO;
+		sut.save(result);
+		verify(dao).insertPackage("g:a", Constants.mvnForge, result.projectName, result.repoUrl, null);
+	}
+
+	@Test
+	public void doNotStoreMavenCentralRepo() {
+		var result = getSomeResult();
+		result.artifactRepository = MavenUtilities.MAVEN_CENTRAL_REPO;
+		sut.save(result);
+		verify(dao, times(0)).insertArtifactRepository(anyString());
+	}
+
+	@Test
+	public void storeNonMavenCentralRepo() {
+		var result = getSomeResult();
+		result.artifactRepository = "Non Maven Central";
+		sut.save(result);
+		verify(dao).insertArtifactRepository(result.artifactRepository);
+	}
+
+	@Test
+	public void storePackageVersion() {
+		var result = getSomeResult();
+		when(dao.insertPackage(anyString(), anyString(), anyString(), anyString(), eq(null))).thenReturn(123L);
+		when(dao.insertArtifactRepository(anyString())).thenReturn(234L);
+		sut.save(result);
+
+		var captor = ArgumentCaptor.forClass(JSONObject.class);
+
+		verify(dao).insertPackageVersion(eq(123L), eq(Constants.opalGenerator), eq(result.version), eq(234L), eq(null),
+				eq(new Timestamp(result.releaseDate)), captor.capture());
+
+		var actualJson = captor.getValue();
+		var expectedJson = DatabaseUtils.toJson(result);
+		assertEquals(expectedJson.toString(), actualJson.toString());
+	}
+
+	@Test
+	public void storeDependency() {
+		var result = getSomeResult();
+
+		when(dao.insertPackage(anyString(), anyString())).thenReturn(123L);
+		when(dao.insertPackageVersion(anyLong(), anyString(), anyString(), anyLong(), eq(null), any(Timestamp.class),
+				any(JSONObject.class))).thenReturn(234L);
+
+		sut.save(result);
+
+		var arrCaptor = ArgumentCaptor.forClass(String[].class);
+		var jsonCaptor = ArgumentCaptor.forClass(JSONObject.class);
+
+		verify(dao).insertPackage("dg1:da1", Constants.mvnForge);
+		verify(dao).insertDependency(eq(234L), eq(123L), arrCaptor.capture(), eq(null), eq(null), eq(null),
+				jsonCaptor.capture());
+
+		var actual = arrCaptor.getValue();
+		assertEquals(1, actual.length);
+		assertEquals("dv1", actual[0]);
+
+		var actualJson = jsonCaptor.getValue();
+		var expectedJson = result.dependencies.iterator().next().toJSON();
+		assertEquals(expectedJson.toString(), actualJson.toString());
+	}
+
+	private PomAnalysisResult getSomeResult() {
+		PomAnalysisResult result = new PomAnalysisResult();
+		result.artifactRepository = "...";
+		result.groupId = "g";
+		result.artifactId = "a";
+		result.packagingType = "jar";
+		result.version = "1.2.3";
+
+		result.projectName = "projectName";
+		result.repoUrl = "repoUrl";
+		result.commitTag = "commitTag";
+		result.dependencies.add(new Dependency("dg1", "da1", "dv1"));
+		result.dependencyManagement.add(new Dependency("dmg1", "dma1", "dmv1"));
+
+		result.releaseDate = new Date().getTime();
+		result.forge = Constants.mvnForge;
+		result.parentCoordinate = "pg:pa:pom:2.3.4";
+		result.sourcesUrl = "soruceUrl";
+		return result;
 	}
 }
