7fce58271036cb2c4e5a542aa4c2351b9650a66b
romatallinn <sirokov@dizconto.com>
2022-01-17 13:36:14 +0100

    Revert "Replace custom GSON builder with the one from VulnerableCallChainJsonUtils"
    This reverts commit fc8ad4ed3d63e8c6ce0b04acfc703cd2a9fb6180.

--- analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/api/impl/VulnerableCallChainsApiServiceImpl.java
+++ analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/api/impl/VulnerableCallChainsApiServiceImpl.java
@@ -18,11 +18,12 @@
 
 package eu.fasten.analyzer.restapiplugin.mvn.api.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import eu.fasten.analyzer.restapiplugin.mvn.KnowledgeBaseConnector;
 import eu.fasten.analyzer.restapiplugin.mvn.api.VulnerableCallChainsApiService;
 import eu.fasten.core.data.FastenURI;
 import eu.fasten.core.vulchains.VulnerableCallChain;
-import eu.fasten.core.vulchains.VulnerableCallChainJsonUtils;
 import eu.fasten.core.vulchains.VulnerableCallChainRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,6 +32,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Set;
 
 
@@ -52,32 +54,36 @@ public class VulnerableCallChainsApiServiceImpl implements VulnerableCallChainsA
     }
 
     /**
-     * Helper function that prepares an HTTP response for the requested vulnerable call chains.
-     * @param chains - the set of vulnerable call chains to be included in the response.
-     * @return the response entity of 200 status and the call chains as JSON.
+     * Helper method to convert set of vulnerable call chains to JSON formatted string response.
+     * @param chains - a set of {@link VulnerableCallChain} objects to be serialized.
+     * @return {@link String} formatted as JSON response.
      */
-    private ResponseEntity<String> prepareResponse(Set<VulnerableCallChain> chains) {
-        var result = VulnerableCallChainJsonUtils.toJson(chains);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    private String VulnerableCallChainsToJSON(Set<VulnerableCallChain> chains) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        return gson.toJson(chains);
     }
 
     @Override
     public ResponseEntity<String> getChainsForPackage(String forge, String packageName, String packageVersion) {
         Set<VulnerableCallChain> chains = vulnerableCallChainRepository.getChainsForPackage(packageName, packageVersion);
-        return prepareResponse(chains);
+        var result = VulnerableCallChainsToJSON(chains);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<String> getChainsForModule(String forge, String packageName, String packageVersion, String rawPath) {
         FastenURI fastenUri = FastenURI.create(forge, packageName, packageVersion, rawPath);
         Set<VulnerableCallChain> chains = vulnerableCallChainRepository.getChainsForModule(fastenUri);
-        return prepareResponse(chains);
+        var result = VulnerableCallChainsToJSON(chains);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<String> getChainsForCallable(String forge, String packageName, String packageVersion, String rawPath) {
         FastenURI fastenUri = FastenURI.create(forge, packageName, packageVersion, rawPath);
         Set<VulnerableCallChain> chains = vulnerableCallChainRepository.getChainsForCallable(fastenUri);
-        return prepareResponse(chains);
+        var result = VulnerableCallChainsToJSON(chains);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
