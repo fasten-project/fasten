a4156f6038671e58bec3ddc17e716f0eff904b2f
Sebastian Proksch <development@mail.proks.ch>
2022-01-27 01:34:11 +0100

    Adopt field name in input data to OPALPlugin and RepoClonerPlugin

--- analyzer/javacg-opal/src/main/java/eu/fasten/analyzer/javacgopal/OPALPlugin.java
+++ analyzer/javacg-opal/src/main/java/eu/fasten/analyzer/javacgopal/OPALPlugin.java
@@ -21,7 +21,6 @@ package eu.fasten.analyzer.javacgopal;
 import static eu.fasten.analyzer.javacgopal.data.CGAlgorithm.CHA;
 import static eu.fasten.analyzer.javacgopal.data.CallPreservationStrategy.ONLY_STATIC_CALLSITES;
 
-import eu.fasten.core.data.PartialJavaCallGraph;
 import java.io.File;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -38,6 +37,7 @@ import org.slf4j.LoggerFactory;
 import eu.fasten.analyzer.javacgopal.data.OPALPartialCallGraphConstructor;
 import eu.fasten.core.data.Constants;
 import eu.fasten.core.data.JSONUtils;
+import eu.fasten.core.data.PartialJavaCallGraph;
 import eu.fasten.core.data.opal.MavenCoordinate;
 import eu.fasten.core.data.opal.exceptions.EmptyCallGraphException;
 import eu.fasten.core.data.opal.exceptions.MissingArtifactException;
@@ -82,7 +82,7 @@ public class OPALPlugin extends Plugin {
             try {
                 // Generate CG and measure construction duration.
                 logger.info("[CG-GENERATION] [UNPROCESSED] [-1] [" + mavenCoordinate.getCoordinate() + "] [NONE] ");
-                long date = kafkaConsumedJson.optLong("date", -1);
+                long date = kafkaConsumedJson.optLong("releaseDate", -1);
 				this.graph = OPALPartialCallGraphConstructor.createExtendedRevisionJavaCallGraph(mavenCoordinate,
                         CHA, date, artifactRepository, ONLY_STATIC_CALLSITES);
                 long endTime = System.nanoTime();
