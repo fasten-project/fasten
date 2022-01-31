f489210de75c09aea6f32fbcbc1a8177474e419b
romatallinn <sirokov@dizconto.com>
2022-01-31 09:47:29 +0100

    Remove old Vulnerability Cache retrieve endpoint from Rest API
    This reverts commit f54c062ab0ccb31c7d2179617e730685d133f49b.

--- analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/api/ResolutionApiService.java
+++ analyzer/restapi-plugin/src/main/java/eu/fasten/analyzer/restapiplugin/mvn/api/ResolutionApiService.java
@@ -31,6 +31,4 @@ public interface ResolutionApiService {
 
     ResponseEntity<String> getDirectedGraph(long packageVersionId, boolean needStitching, long timestamp);
 
-    ResponseEntity<String> getTransitiveVulnerabilities(String package_name, String version);
-
 }
