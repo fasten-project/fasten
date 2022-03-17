73c3cea1bf5bdc271eb99c4e805cdbcbec4565cd
Michele Scarlato <michele.scarlato@gmail.com>
2022-03-17 10:16:47 +0100

    adding DebianProduct and Revision

---
+++ core/src/main/java/eu/fasten/core/debian/data/DebianProduct.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.fasten.core.debian.data;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * A versionless Debian artifact
+ */
+
+public class DebianProduct {
+    public long id;
+    //maybe change the name
+    public String artifactId;
+    public DebianProduct() {}
+
+    public DebianProduct(final String artifactId) {
+        this.id = 0;
+        this.artifactId = artifactId;
+    }
+    public DebianProduct(long id, String artifactId) {
+        this.id = id;
+        this.artifactId = artifactId;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((artifactId == null) ? 0 : artifactId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DebianProduct other = (DebianProduct) obj;
+        if (artifactId == null) {
+            if (other.artifactId != null)
+                return false;
+        } else if (!artifactId.equals(other.artifactId))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+}
---
+++ core/src/main/java/eu/fasten/core/debian/data/Revision.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.fasten.core.debian.data;
+
+import eu.fasten.core.data.Constants;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+/**
+ * An artifact released in Debian packages.
+ */
+public class Revision extends DebianProduct implements Serializable {
+    // I am not sure this reflect the current Debian structure
+    private static final long serialVersionUID = 3096663425693127578L;
+
+    public DefaultArtifactVersion version;
+    public Timestamp createdAt;
+
+    public Revision() {
+    }
+
+    public Revision(final String artifactId, final String version, final Timestamp createdAt) {
+        super(artifactId);
+
+        this.version = new DefaultArtifactVersion(version);
+        this.createdAt = createdAt;
+    }
+
+    public Revision(final long id, final String artifactId,
+                    final String version, final Timestamp createdAt) {
+        super(id, artifactId);
+
+        this.version = new DefaultArtifactVersion(version);
+        this.createdAt = createdAt;
+    }
+    public DebianProduct product() {
+        return new DebianProduct(artifactId);
+    }
+
+    public JSONObject toJSON() {
+        var json = new JSONObject();
+        json.put("id", id);
+        json.put("artifactId", artifactId);
+        json.put("version", version.toString());
+        json.put("createdAt", createdAt.getTime());
+        return json;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((version == null) ? 0 : version.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Revision other = (Revision) obj;
+        if (version == null) {
+            if (other.version != null)
+                return false;
+        } else if (!version.equals(other.version))
+            return false;
+        return true;
+    }
+}
+
