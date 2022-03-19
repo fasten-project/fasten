a69f47965701b180499b54b1d4942676e1dad65d
Sebastian Proksch <development@mail.proks.ch>
2022-03-19 22:56:54 +0100

    Remove irrelevant parts of DependencyGraphBuilder

--- core/src/main/java/eu/fasten/core/maven/resolution/DependencyGraphBuilder.java
+++ core/src/main/java/eu/fasten/core/maven/resolution/DependencyGraphBuilder.java
@@ -29,36 +29,47 @@ import java.util.stream.Collectors;
 
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.jooq.DSLContext;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eu.fasten.core.maven.data.Dependency;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import eu.fasten.core.json.ObjectMapperBuilder;
 import eu.fasten.core.maven.data.PomAnalysisResult;
 import eu.fasten.core.maven.data.VersionConstraint;
 
 public class DependencyGraphBuilder {
 
     private static final Logger logger = LoggerFactory.getLogger(DependencyGraphBuilder.class);
+    private static final ObjectMapper OM = new ObjectMapperBuilder().build();
+
+    private DSLContext dbContext;
+    private MavenDependentsGraph dataDpdt;
+    private MavenDependencyGraph dataDeps;
 
     public static IMavenResolver init(DSLContext dbContext, String path) {
+        assertNotNullOrEmpty(path);
+        var builder = new DependencyGraphBuilder(dbContext);
+
+        // TODO clean-up method
         try {
-            MavenDependentsGraph graph;
             if (DependencyIOUtils.doesDependentsGraphExist(path)) {
-                graph = DependencyIOUtils.loadDependentsGraph(path);
-            
+//                dataDpdt = DependencyIOUtils.loadDependentsGraph(path);
+
             } else {
-                assertNotNullOrEmpty(path);
                 logger.info("Building dependency graph ...");
-                var graphBuilder = new DependencyGraphBuilder();
-                graph = graphBuilder.buildDependentsGraph(dbContext);
+                builder.build();
 
                 logger.info("Serializing graph to {}", path);
-                //DependencyIOUtils.serializeDependentsGraph(graph, path);
-                
+                // DependencyIOUtils.serializeDependentsGraph(graph, path);
+
                 logger.info("Finished serializing graph");
             }
-            return new MavenResolver(graph);
+            var resolverDeps = new MavenDependencyResolver();
+            resolverDeps.setData(builder.dataDeps);
+
+            return new MavenResolver(new MavenDependentsResolver(builder.dataDpdt), resolverDeps);
         } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
@@ -66,18 +77,21 @@ public class DependencyGraphBuilder {
         }
     }
 
-    public MavenDependentsGraph buildDependentsGraph(DSLContext dbContext) {
-        logger.info("Retrieving package versions ...");
-        var poms = getPomAnalysisResults(dbContext);
-        logger.info("Found {} package versions.", poms.size());
-
-        var dependents = new MavenDependentsGraph();
-        poms.forEach(pom -> dependents.add(pom));
-
-        return dependents;
+    public DependencyGraphBuilder(DSLContext dbContext) {
+        this.dbContext = dbContext;
     }
-    
-    public Set<PomAnalysisResult> getPomAnalysisResults(DSLContext dbContext) {
+
+    private void build() {
+
+        dataDpdt = new MavenDependentsGraph();
+        dataDeps = new MavenDependencyGraph();
+        for (var pom : getPomAnalysisResults()) {
+            dataDpdt.add(pom);
+            dataDeps.add(pom);
+        }
+    }
+
+    public Set<PomAnalysisResult> getPomAnalysisResults() {
 
         var dbRes = dbContext.select( //
                 PACKAGE_VERSIONS.METADATA, //
@@ -88,93 +102,16 @@ public class DependencyGraphBuilder {
 
         var pars = dbRes.stream() //
                 .map(x -> {
-                    var json = new JSONObject(x.component1().data());
-
-                    var par = new PomAnalysisResult();
-                    par.id = x.component2();
-                    par.groupId = json.getString("groupId");
-                    par.artifactId = json.getString("artifactId");
-                    par.version = json.getString("version");
-                    par.releaseDate = json.getLong("releaseDate");
-
-                    var deps = json.getJSONArray("dependencies");
-                    for (var i = 0; i < deps.length(); i++) {
-                        var dep = deps.getJSONObject(i);
-                        par.dependencies.add(Dependency.fromJSON(dep));
+                    try {
+                        var json = x.component1().data();
+                        var pom = OM.readValue(json, PomAnalysisResult.class);
+                        pom.id = x.component2();
+                        return pom;
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
                     }
-
-                    var mgts = json.getJSONArray("dependencyManagement");
-                    for (var i = 0; i < mgts.length(); i++) {
-                        var mgt = mgts.getJSONObject(i);
-                        par.dependencyManagement.add(Dependency.fromJSON(mgt));
-                    }
-
-                    return par;
                 }).collect(Collectors.toSet());
 
         return pars;
-    }
-
-    public List<PomAnalysisResult> findMatchingRevisions(Collection<PomAnalysisResult> revisions,
-            Set<VersionConstraint> constraints) {
-        if (revisions == null) {
-            return Collections.emptyList();
-        }
-        return revisions.stream() //
-                .filter(r -> DependencyGraphBuilder.isMatch(r, constraints)) //
-                .collect(Collectors.toList());
-    }
-
-    private static boolean isMatch(PomAnalysisResult r, Set<VersionConstraint> constraints) {
-        var version = new DefaultArtifactVersion(r.version);
-        for (var constraint : constraints) {
-            if (isHardConstraint(constraint)) {
-                if (checkVersionLowerBound(constraint, version) && checkVersionUpperBound(constraint, version)) {
-                    return true;
-                }
-            } else {
-                if (isSimpleVersion(constraint) && new DefaultArtifactVersion(constraint.lowerBound).equals(version)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private static boolean isSimpleVersion(VersionConstraint constraint) {
-        return constraint.lowerBound.equals(constraint.upperBound);
-    }
-
-    private static boolean isHardConstraint(VersionConstraint constraint) {
-        var spec = constraint.toString();
-        boolean isHardConstraint = (spec.startsWith("[") || spec.startsWith("("))
-                && (spec.endsWith("]") || spec.endsWith(")"));
-        return isHardConstraint;
-    }
-
-    private static boolean checkVersionLowerBound(VersionConstraint constraint, DefaultArtifactVersion version) {
-        if (constraint.lowerBound.isEmpty()) {
-            return true;
-        }
-        if (constraint.isLowerBoundInclusive) {
-            return version.compareTo(new DefaultArtifactVersion(constraint.lowerBound)) >= 0;
-        } else {
-            return version.compareTo(new DefaultArtifactVersion(constraint.lowerBound)) > 0;
-        }
-    }
-
-    private static boolean checkVersionUpperBound(VersionConstraint constraint, DefaultArtifactVersion version) {
-        if (constraint.upperBound.isEmpty()) {
-            return true;
-        }
-        if (constraint.isUpperBoundInclusive) {
-            return version.compareTo(new DefaultArtifactVersion(constraint.upperBound)) <= 0;
-        } else {
-            return version.compareTo(new DefaultArtifactVersion(constraint.upperBound)) < 0;
-        }
-    }
-
-    private static String strip(String s) {
-        return s.replaceAll("[\\n\\t ]", "");
     }
 }
