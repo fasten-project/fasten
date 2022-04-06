0dbcdf746a429c99bb8e7018fe476c97c7c9ae72
Sebastian Proksch <development@mail.proks.ch>
2022-04-06 02:26:40 +0200

    Added test for MavenDependentsResolver that cover time and version ranges.

---
+++ core/src/test/java/eu/fasten/core/maven/resolution/AbstractMavenDependentsResolverTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2022 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.fasten.core.maven.resolution;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import eu.fasten.core.maven.data.Revision;
+
+public class AbstractMavenDependentsResolverTest {
+
+    protected static final String DEST = "dest:1";
+
+    protected MavenDependentsData data;
+    protected MavenDependentsResolver sut;
+
+    protected ResolverConfig config;
+
+    @BeforeEach
+    public void setupAbstract() {
+        data = new MavenDependentsData();
+        sut = new MavenDependentsResolver(data);
+        config = new ResolverConfig();
+    }
+
+    protected void assertDependents(String shortTarget, String... depSet) {
+        var targetParts = shortTarget.split(":");
+        var target = String.format("%s:%s:%s", targetParts[0], targetParts[0], targetParts[1]);
+        var actuals = sut.resolve(target, config);
+        var expecteds = Arrays.stream(depSet) //
+                .map(gav -> gav.split(":")) //
+                .map(parts -> new Revision(parts[0], parts[0], parts[1], new Timestamp(-1L))) //
+                .collect(Collectors.toSet());
+
+        if (!expecteds.equals(actuals)) {
+            var sb = new StringBuilder();
+            sb.append("Expected:\n");
+            for (var e : expecteds) {
+                sb.append("- ").append(e.groupId).append(":").append(e.artifactId).append(":").append(e.version)
+                        .append("\n");
+            }
+            sb.append("But was:\n");
+            for (var a : actuals) {
+                sb.append("- ").append(a.groupId).append(":").append(a.artifactId).append(":").append(a.version)
+                        .append("\n");
+            }
+            fail(sb.toString());
+        }
+    }
+}
---
+++ core/src/test/java/eu/fasten/core/maven/resolution/MavenDependentsResolverTimeTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2022 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.fasten.core.maven.resolution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import eu.fasten.core.maven.data.Dependency;
+import eu.fasten.core.maven.data.Pom;
+
+public class MavenDependentsResolverTimeTest extends AbstractMavenDependentsResolverTest {
+
+    private static final long SOME_TIME = 1234;
+
+    @Test
+    public void destDoesNotExistYet() {
+        add(0, DEST);
+        config.resolveAt = SOME_TIME - 1;
+        var e = assertThrows(MavenResolutionException.class, () -> {
+            sut.resolve("dest:dest:1", config);
+        });
+        assertEquals("Cannot find coordinate dest:dest:1", e.getMessage());
+    }
+
+    @Test
+    public void direct() {
+        add(0, DEST);
+        add(0, "a:1", DEST);
+        assertDependentsAt(0, DEST, "a:1");
+    }
+
+    @Test
+    public void directTooNew() {
+        add(0, DEST);
+        add(1, "a:1", DEST);
+        assertDependentsAt(0, DEST);
+    }
+
+    @Test
+    public void directTooNewWithTransitive() {
+        add(0, DEST);
+        add(1, "a:1", DEST);
+        add(0, "b:1", "a:1");
+        assertDependentsAt(0, DEST);
+    }
+
+    @Test
+    public void transitive() {
+        add(0, DEST);
+        add(0, "a:1", DEST);
+        add(0, "b:1", "a:1");
+        assertDependentsAt(0, DEST, "a:1", "b:1");
+    }
+
+    @Test
+    public void transitiveToNew() {
+        add(0, DEST);
+        add(0, "a:1", DEST);
+        add(1, "b:1", "a:1");
+        assertDependentsAt(0, DEST, "a:1");
+    }
+
+    private void add(long releasedAtDelta, String from, String... tos) {
+        var pom = new Pom();
+        var parts = from.split(":");
+        pom.groupId = parts[0];
+        pom.artifactId = parts[0];
+        pom.version = parts[1];
+        pom.releaseDate = SOME_TIME + releasedAtDelta;
+
+        for (var to : tos) {
+            var partsTo = to.split(":");
+            var d = new Dependency(partsTo[0], partsTo[0], partsTo[1]);
+
+            pom.dependencies.add(d);
+        }
+
+        data.add(pom);
+    }
+
+    private void assertDependentsAt(long resolveAtDelta, String shortTarget, String... depSet) {
+        config.resolveAt = SOME_TIME + resolveAtDelta;
+        assertDependents(shortTarget, depSet);
+    }
+}
