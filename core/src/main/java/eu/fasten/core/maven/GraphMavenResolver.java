a3a213282ef85be403c5c1dfbc50f7c769675f1f
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2022-04-24 09:50:04 +0200

    Added try/catch

--- core/src/main/java/eu/fasten/core/maven/GraphMavenResolver.java
+++ core/src/main/java/eu/fasten/core/maven/GraphMavenResolver.java
@@ -442,28 +442,27 @@ public class GraphMavenResolver implements Runnable {
 
         ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
         Future<?> future = singleThreadExecutor.submit(() -> {
-				var countDown = maxDeps;
+			var countDown = maxDeps;
+			try {
 				while (!workQueue.isEmpty()) {
 					var rev = workQueue.dequeue();
 					if (!dependentGraph.containsVertex(rev)) continue; // Ignore missing revisions
 					try {
 						result.put(rev);
 						if (countDown-- == 0) break;
-					} catch (InterruptedException cantHappen) {
-					}
+					} catch (InterruptedException cantHappen) {}
 
 					filterDependentsByTimestamp(StreamSupport.stream(dependentGraph.iterables().outgoingEdgesOf(rev).spliterator(), false).map(edge -> dependentGraph.getEdgeTarget(edge)), timestamp).forEach(dependent -> {
 						if (seen.add(dependent.id)) workQueue.enqueue(dependent);
 					});
 					if (!transitive) break;
-				}
-
+				} 
+			} finally {
 				try {
 					for (int i = 0; i < numberOfThreads; i++) result.put(END);
-				} catch (InterruptedException cantHappen) {
-				}
+				} catch (InterruptedException cantHappen) {}
 			}
-		);
+		});
         singleThreadExecutor.shutdown();
         return future;
     }
