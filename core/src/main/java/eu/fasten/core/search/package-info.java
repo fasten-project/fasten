c63a9b89b865942fb6624370b15cfb4f9e2b818f
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2022-05-26 12:02:12 +0200

    Javadoc

--- core/src/main/java/eu/fasten/core/search/package-info.java
+++ core/src/main/java/eu/fasten/core/search/package-info.java
@@ -1,3 +1,12 @@
-/** A search engine over the FASTEN Knowledge base. */
+/** <h1>A search engine over the FASTEN knowledge base</h1> 
+ * 
+ * <p>This package contains an implementation of a ranked search engine over the FASTEN knowledge base.
+ * The engine makes it possible to query for (co)reachable methods, possibly filtered using JSON
+ * queries over metadata (e.g., information about vulnerabilities or licensing).
+ * 
+ * <p>The {@linkplain eu.fasten.core.search.SearchEngine class documentation} shows in detail
+ * how to access the search engine features, and Deliverable 5.5 explains in some detail its design.
+ * 
+ */
 
 package eu.fasten.core.search;
